54774a118cfb22975b8ab313f70a5119c3995340
Himanshu Bhagat <himanshubhagat080104@gmail.com>
2026-06-10 14:08:03 +0530

    updated cors

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Next.js local
+        "https://blogify-frontend-sigma.vercel.app/",  # Next.js deployed
     ],
     allow_credentials=True,
     allow_methods=["*"],
